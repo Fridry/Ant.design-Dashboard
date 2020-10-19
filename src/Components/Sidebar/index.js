4de531807cbbf3171f0ccb42a2696fcd021ab1b5
Fridry <fredwin.ed@gmai.com>
2020-10-19 16:14:54 -0300

    Ajustes na tabela de Empresas e adição da função de exclusão

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -11,13 +11,15 @@ import {
 } from "@ant-design/icons";
 
 import "./styles.css";
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
 const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
+
+  let location = useLocation();
 
   const onCollapse = (collapsed) => {
     setCollapsed(collapsed);
@@ -28,34 +30,40 @@ const Sidebar = () => {
       <div className="logo">
         <CaretRightOutlined />
       </div>
-      <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-        <Menu.Item key="1" icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
+      <Menu
+        theme="dark"
+        defaultSelectedKeys={["/"]}
+        selectedKeys={[location.pathname]}
+        defaultOpenKeys={["sub1"]}
+        mode="inline"
+      >
+        <Menu.Item key="/" icon={<HomeOutlined />}>
+          <NavLink to="/">Home</NavLink>
         </Menu.Item>
 
         <SubMenu key="sub1" icon={<ShopOutlined />} title="Empresas">
-          <Menu.Item key="3">
-            <Link to="/company/new">Cadastrar empresa</Link>
+          <Menu.Item key="/company/new">
+            <NavLink to="/company/new">Cadastrar empresa</NavLink>
           </Menu.Item>
-          <Menu.Item key="4">
-            <Link to="/company/list">Listar empresas</Link>
+          <Menu.Item key="/company/list">
+            <NavLink to="/company/list">Listar empresas</NavLink>
           </Menu.Item>
         </SubMenu>
         <SubMenu key="sub2" icon={<GroupOutlined />} title="Unidades">
-          <Menu.Item key="6">Team 1</Menu.Item>
-          <Menu.Item key="8">Team 2</Menu.Item>
+          <Menu.Item key="4">Team 1</Menu.Item>
+          <Menu.Item key="5">Team 2</Menu.Item>
         </SubMenu>
         <SubMenu key="sub3" icon={<SettingOutlined />} title="Ativos">
           <Menu.Item key="6">Team 1</Menu.Item>
-          <Menu.Item key="8">Team 2</Menu.Item>
+          <Menu.Item key="7">Team 2</Menu.Item>
         </SubMenu>
         <SubMenu key="sub4" icon={<TeamOutlined />} title="Usuários">
-          <Menu.Item key="6">Team 1</Menu.Item>
-          <Menu.Item key="8">Team 2</Menu.Item>
+          <Menu.Item key="8">Team 1</Menu.Item>
+          <Menu.Item key="9">Team 2</Menu.Item>
         </SubMenu>
         <SubMenu key="sub5" icon={<ProfileOutlined />} title="Categorias">
-          <Menu.Item key="6">Team 1</Menu.Item>
-          <Menu.Item key="8">Team 2</Menu.Item>
+          <Menu.Item key="10">Team 1</Menu.Item>
+          <Menu.Item key="11">Team 2</Menu.Item>
         </SubMenu>
       </Menu>
     </Sider>
--- src/Pages/Company/List/index.js
+++ src/Pages/Company/List/index.js
@@ -1,14 +1,132 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import {
+  Button,
+  Row,
+  Col,
+  Table,
+  Space,
+  Popconfirm,
+  message,
+  Typography,
+} from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
 import Layout from "../../../Components/Layout";
+import api from "../../../services/api";
+
+const { Title } = Typography;
 
 const List = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("/company")
+      .then((response) => {
+        setData(
+          response.data.map((row) => ({
+            key: row._id,
+            name: row.name,
+            email: row.email,
+            phone: row.phoneNumber,
+            address: row.address,
+            city: row.city,
+          }))
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [data]);
+
+  const handleDelete = async (key) => {
+    try {
+      await api.delete(`/company/${key}`);
+
+      setData(data.filter((d) => d.key !== key));
+
+      message.info("Registro excluido com sucesso.");
+    } catch (error) {
+      message.error("Erro ao excluir registro, tente novamente.");
+    }
+  };
+
+  const columns = [
+    {
+      title: "Nome",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Telefone",
+      dataIndex: "phone",
+      key: "phone",
+    },
+    {
+      title: "Endereço",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "Cidade",
+      dataIndex: "city",
+      key: "city",
+    },
+    {
+      title: "Ações",
+      dataIndex: "actions",
+      key: "actions",
+      render: (text, record) =>
+        data.length >= 1 ? (
+          <Space size="middle" align="center">
+            <Button
+              type="primary"
+              icon={<EditOutlined />}
+              size={25}
+              style={{ background: "#ffec3d", border: "none" }}
+              title="Editar"
+            />
+
+            <Popconfirm
+              title="Deseja excluir este registro?"
+              onConfirm={() => handleDelete(record.key)}
+            >
+              <Button
+                type="primary"
+                danger
+                icon={<DeleteOutlined />}
+                title="Excluir"
+              />
+            </Popconfirm>
+          </Space>
+        ) : null,
+    },
+  ];
+
   return (
-    <div>
-      <Layout>
-        <h1>List</h1>
-      </Layout>
-    </div>
+    <Layout>
+      <Row>
+        <Col span={24}>
+          <Table
+            dataSource={data}
+            columns={columns}
+            bordered={true}
+            title={() => (
+              <Title style={{ textAlign: "center" }} level={3}>
+                Empresas cadastradas
+              </Title>
+            )}
+            size="small"
+          />
+        </Col>
+      </Row>
+    </Layout>
   );
 };
 
